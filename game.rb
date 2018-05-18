5b5c26b2e47d6c574259eba06bf6411229ba8cea
wilmer142 <wilmercollege@gmail.com>
2018-05-17 21:13:50 -0500

    Creating game method and messages

--- game.rb
+++ game.rb
@@ -1,41 +1,73 @@
-require_relative "questions.rb"
+require "./questions.rb"
 require "pry"
 
-module Game
 
-	include Question
+include Question
 
-	class Game
+class Game
 
-		def initialize
-			@questions = Question.new
-			@right_answers = 0
-			show_welcome_message
-			start_game
-		end
+	def initialize
+		@questions = Question.new
+		@right_answers = 0
+		@tries = 0
+		show_welcome_message
+		start_game
+		final_message
+	end
 
-		def start_game
-			while @right_answers < @questions.questions.length
-				@questions.questions.each do |question|
-					
+	def start_game
+		while @right_answers < @questions.questions.length
+			@questions.questions.each do |question|
+				again = true
+				while again
+					@tries += 1
+					answer = show_question(question[:question])
+					if answer == question[:answer]
+						question[:answered] = true
+						@right_answers += 1
+					else
+						next_answer = wrong_answer
+						again = next_answer == 1 ? true : false
+					end
 				end
 			end
 		end
-
-		private
-
-			def show_welcome_message
-				system "clear"
-				puts "------ ¡Bienvenido a Trivia Ruby ------"
-				puts ""
-				puts "-- Reglas: "
-				puts "---- 1) Cada pregunta tiene una única respuesta"
-				puts "---- 2) No puede llamar a un amigo"
-				puts "---- 3) No se vale buscar en San Google"
-				puts ""
-				print "Presiona cualquier tecla para continuar... "
-			end
-
 	end
 
+	private
+
+		def show_welcome_message
+			system "clear"
+			puts "------ ¡Bienvenido a Trivia Ruby ------"
+			puts ""
+			puts "-- Reglas: "
+			puts "---- 1) Cada pregunta tiene una única respuesta"
+			puts "---- 2) No puede llamar a un amigo"
+			puts "---- 3) No se vale buscar en San Google"
+			puts ""
+			print "Presiona cualquier tecla para continuar... "
+		end
+
+		def show_question(question)
+			puts "-- Pregunta: "
+			puts "---- #{question}"
+			print "Tu respuesta: "
+			gets.chomp
+		end
+
+		def wrong_answer
+			puts "** Respuesta incorrecta **"
+			puts "----1) Reintentar"
+			puts "----2) Continuar"
+			print "Elegir: "
+			gets.chomp
+		end
+
+		def final_message
+			puts "Felicidades! has contestado correctamente todas las preguntas!"
+			puts "Total de intentos: #{@tries}"
+		end
+
 end
+
+game = Game.new
